115c134d9aa5ab7a15859cadda1ef4a932a4dfe5
Akuseru_W <wolfs.axelw@gmail.com>
2024-05-18 18:34:50 +0200

    Initialization of the project, addition of GSAP and SASS

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+};
 
 export default nextConfig;
+
+//https://nextjs.org/docs/app/building-your-application/styling/sass
+const currentDirUrl = new URL(import.meta.url);
+const currentDirPath = path.dirname(currentDirUrl.href);
+
+export const sassOptions = {
+    includePaths: [path.join(currentDirPath, 'styles')],
+};
